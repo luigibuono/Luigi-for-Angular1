0b8a813b3076548fa680b8c51a92e0f43e7151f6
Luigi <128727133+luigibuono@users.noreply.github.com>
2023-07-14 13:56:06 +0200

    Add files via upload

---
+++ environments/envionment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  token: '',
+  user: {},
+  userLink: 'https://gorest.co.in/public/v2/users',
+  postLink: 'https://gorest.co.in/public/v2/posts',
+};
---
+++ mysocials/mysocials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mysocials',
+  templateUrl: './mysocials.component.html',
+  styleUrls: ['./mysocials.component.css']
+})
+export class MysocialsComponent {
+
+}
